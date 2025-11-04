569325d7621898967daaadf7100a933b3e81fa59
jun <hellojun82@gmail.com>
2025-11-04 14:51:13 +0900

    t

--- routes/crm/popbill.js
+++ routes/crm/popbill.js
@@ -6,21 +6,43 @@ const router = express.Router();
 
 router.get("/kakao/Templatelist", popbillController.Templatelist);
 router.get("/kakao/TemplateContent", popbillController.TemplateContent);
-router.post("/kakao/MessageSend", popbillController.MessageSend);
-router.get("/kakao/getSendMessages", popbillController.getSendMessages);
-router.get("/kakao/SendMessages", popbillController.getSendMessages);
-router.get("/kakao/SendMessageHistory", popbillController.SendMessageHistory);
-router.get("/kakao/SendMessageHistoryCount", popbillController.SendMessageHistoryCount);
+// router.post("/kakao/MessageSend", popbillController.MessageSend);
+// router.get("/kakao/getSendMessages", popbillController.getSendMessages);
+// router.get("/kakao/SendMessages", popbillController.getSendMessages);
+// router.get("/kakao/SendMessageHistory", popbillController.SendMessageHistory);
+// router.get("/kakao/SendMessageHistoryCount", popbillController.SendMessageHistoryCount);
 
-router.get("/biz/checkBizInfo", popbillController.checkBizInfo);
+// router.get("/biz/checkBizInfo", popbillController.checkBizInfo);
 
-router.post("/tax/registTaxIssue", popbillController.registTaxIssue);
+// router.post("/tax/registTaxIssue", popbillController.registTaxIssue);
 
-router.get("/bank/latestTransactions", popbillController.latestTransactions);
-router.get("/bank/get_DB_BankTransactions", popbillController.get_DB_BankTransactions);
-router.put("/bank/updateMoneyfinish", popbillController.updateMoneyfinish);
-router.post("/bank/bulkUpdate", popbillController.BulkUpdateMemo);
-// router.post("/bank/SingleUpdate", popbillController.SingleUpdate);
-router.put("/bank/:tid/:trserial", popbillController.singleMemoUpdate);
+// router.get("/bank/latestTransactions", popbillController.latestTransactions);
+// router.get("/bank/get_DB_BankTransactions", popbillController.get_DB_BankTransactions);
+// router.put("/bank/updateMoneyfinish", popbillController.updateMoneyfinish);
+// router.post("/bank/bulkUpdate", popbillController.BulkUpdateMemo);
+// router.put("/bank/:tid/:trserial", popbillController.singleMemoUpdate);
 
 export default router;
+
+
+
+
+
+// routes/customers.js
+// import express from "express";
+// import customersController from "../../controllers/crm/customersController.js";
+
+// const router = express.Router();
+
+// //추가
+// router.post("/", customersController.addCustomer);
+// //검색
+// router.get("/", customersController.getCustomers);
+// //ID값검색
+// router.get("/:id", customersController.getCustomerById);
+// //수정
+// router.put("/:id", customersController.updateCustomer);
+// //삭제
+// router.delete("/:id", customersController.deleteCustomer);
+
+// export default router;
